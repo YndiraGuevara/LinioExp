c999b2489acb8d4707ee7d40ccaa53300e7e5478
yndira <ly.guevarap@alum.up.edu.pe>
2021-06-19 01:03:31 -0500

    19-06

--- linioPrueba/settings.py
+++ linioPrueba/settings.py
@@ -73,15 +73,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'linioPrueba.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
+from decouple import config
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
